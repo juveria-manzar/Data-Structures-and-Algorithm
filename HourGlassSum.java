a8b13eabe7e89d6a2edd78535c39714cb08e2a74
Juveria Manzar <juveriamanzar29@gmail.com>
2020-12-20 20:19:24 +0530

    sub-array-sliding-window

--- HourGlassSum.java
+++ HourGlassSum.java
@@ -5,7 +5,7 @@ public class HourGlassSum {
     static int hourglassSum(int[][] arr){
         int rows=arr.length;
         int columns=arr[0].length;
-        int max_hourglass_sum=Integer.MIN_VALUE;
+        int max_hourglass_sum=-9;
         for(int i=0;i<rows-2;i++){
             for(int j=0;j<columns-2;j++){
                 int current_hourglass_sum=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
