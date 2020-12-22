78e2fb81b36540a983f1c688814bf3031776422c
Juveria Manzar <juveriamanzar29@gmail.com>
2020-12-23 04:27:00 +0530

    Largest Sub Array for Sum

--- LargestSubArrayforSum.java
+++ LargestSubArrayforSum.java
@@ -4,15 +4,21 @@ public class LargestSubArrayforSum {
         int curr_sum=0,m_l=0;
         while(j<arr.length){
             curr_sum+=arr[j];
-            if(curr_sum==sum){
+            if(curr_sum<sum){
+                j++;
+            }
+            else if(curr_sum==sum){
                 start=i;
                 m_l=Math.max(m_l, j-i+1);
+                j++;
             }
-            if(curr_sum>sum){
-                curr_sum-=arr[i];
-                i++;
+            else if(curr_sum>sum){
+                while(curr_sum>sum){
+                    curr_sum-=arr[i];
+                    i++;
+                }
+                j++;
             }
-            j++;
         }
         System.out.println(start);
         System.out.println("Sum is found within indices " + start + " and " + m_l);
