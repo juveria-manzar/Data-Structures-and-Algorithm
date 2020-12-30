76424dfaebbfc33e8cf0fe586032228e95cc80d0
Juveria Manzar <juveriamanzar29@gmail.com>
2020-12-30 08:30:04 +0530

    fundamentals

---
+++ Fundamentals/SpanOfArray.java
@@ -0,0 +1,25 @@
+/**
+ * @author Juveria-Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2020-12-30 07:51:15
+ * @modify date 2020-12-30 07:51:15
+ * @desc [description]
+ */
+
+public class SpanOfArray {
+    public static void main(String[] args) {
+        int arr[]=new int[]{15,30,40,4,11,9};
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+            else if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        int result=max-min;
+        System.out.println(result);
+    }
+}
