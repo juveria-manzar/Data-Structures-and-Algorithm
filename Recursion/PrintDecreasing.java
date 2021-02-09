1238df90e2e81884efa31fffc7cdc31b1f8cb35a
JUVERIA MANZAR <Juveria Manzar>
2021-02-09 22:12:27 +0530

    recursion

--- Recursion/PrintDecreasing.java
+++ Recursion/PrintDecreasing.java
@@ -1,13 +1,13 @@
 public class PrintDecreasing {
     public static void printSeries(int n){
-        if(n!=0){
-            System.out.println(n);
-            printSeries(n-1);
+        if(n==0){
+            return;
         }
+        System.out.println(n);
+        printSeries(n-1);
     }
  public static void main(String[] args) {
-     int n=6;
-    //  System.out.println(n);
-     printSeries(n);
+    int n=6;
+    printSeries(n);
  }   
 }
