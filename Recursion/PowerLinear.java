3eab18a5514839e9e22059bb3ba44ddc5a02a6a7
JUVERIA MANZAR <Juveria Manzar>
2021-02-09 23:03:42 +0530

    recursion

---
+++ Recursion/PowerLinear.java
@@ -0,0 +1,17 @@
+public class PowerLinear {
+    public static int power(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        int xnm1=power(x, n-1);
+        int xn=xnm1*x;
+        return xn;
+    }
+    public static void main(String[] args) {
+        int x=3;
+        int n=4;
+        int ans=power(x,n);
+        System.out.println(ans);
+        
+    }
+}
