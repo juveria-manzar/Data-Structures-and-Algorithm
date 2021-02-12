e9d6400b4e6ff2d6d3c716a5b32aeb29611b6b9b
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-02-13 00:22:15 +0530

    TwoPointers-Remove Duplicates from sorted array

--- Two Pointers/RemoveDuplicates.java
+++ Two Pointers/RemoveDuplicates.java
@@ -2,14 +2,18 @@
 public class RemoveDuplicates {
     static int removeDuplicates(int[] arr,int n){
         int newIndex=0;
-        // arr[]={1, 2, 2, 3, 4, 4, 4, 5, 5};
+
+        if(n==0||n==1){
+            return n;
+        }
         for(int i=0;i<n-1;i++){
             if(arr[i]!=arr[i+1]){
                 arr[newIndex]=arr[i];
                 newIndex++;
             }
         }
-        arr[newIndex]=arr[n-1];
+        // System.out.println(arr[n-1]);
+        arr[newIndex++]=arr[n-1];
         return newIndex;
     }
     public static void main(String[] args) {
