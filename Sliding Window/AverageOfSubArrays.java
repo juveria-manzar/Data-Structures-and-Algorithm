9a9ab1c698d1e3d5b35458828d6371091ecca1fd
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-02-15 20:48:09 +0530

    sliding-window

--- Sliding Window/AverageOfSubArrays.java
+++ Sliding Window/AverageOfSubArrays.java
@@ -1,16 +1,9 @@
 /**
- * @author Juveria-Manzar
+ * @author Juveria Manzar
  * @email juveriamanzar29@gmail.com
- * @create date 2020-12-20 19:36:06
- * @modify date 2020-12-20 19:36:06
- * @desc Given an unsorted array A of size N of non-negative integers, find a continuous sub-array which adds to a given number S.
-
-Input:
-The first line of input contains an integer T denoting the number of test cases. 
-Then T test cases follow. Each test case consists of two lines. The first line of each test case is N and S, where N is the size of array and S is the sum. The second line of each test case contains N space separated integers denoting the array elements.
-
-Output:
-For each testcase, in a new line, print the starting and ending positions(1 indexing) of first such occuring subarray from the left if sum equals to subarray, else print -1.
+ * @create date 2021-02-15 20:41:19
+ * @modify date 2021-02-15 20:41:19
+ * @desc [description]
  */
 
  //Efficient Approach-Sliding-window
