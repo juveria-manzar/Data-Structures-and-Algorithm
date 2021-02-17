bddb774f607ec4882131210c100d6d0cc9d06da3
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-02-17 21:18:07 +0530

    fibonacci-recursion

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -7,8 +7,14 @@
  * Recursive TC: O(2^n)
  */
 public class Fibonacci {
-    
+    static int fib(int n){
+        if(n<=1){
+            return n;
+        }
+        return fib(n-2) + fib(n-1);
+    }
     public static void main(String[] args) {
-        
+        int n=5;
+        System.out.println(fib(n));
     }
 }
