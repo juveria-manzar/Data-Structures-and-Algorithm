f0143ee7ef8c5ba5b9b5d6db2d4732b3b67368dc
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-02-17 19:36:14 +0530

    basic-recursion

---
+++ Recursion/TaylorSeriesLoop.java
@@ -0,0 +1,26 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-02-17 19:25:12
+ * @modify date 2021-02-17 19:25:12
+ * @desc e^x=1+x[1+x/2[1+x/3[1+x/4]]]
+ * utilizing calling time recursion and hence can be easily written with loops.
+ * time complexity: O(n)
+ */
+
+public class TaylorSeriesLoop {
+    private static float findTaylorSeries(int n,float x){
+        float result=1;
+        for(;n>0;n--){
+            result=1+x/n*result;
+        }
+
+        return result;
+
+    }
+    public static void main(String[] args) {
+        int n=25;
+        float x=5.0f;
+        System.out.println(findTaylorSeries(n,x));
+    }
+}
