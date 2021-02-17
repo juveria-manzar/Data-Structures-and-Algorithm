ea87d5a4cec594350a9084d42a25c547f19bd9bd
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-02-17 20:36:26 +0530

    basic-recursion

--- Recursion/TaylorSeriesLoop.java
+++ Recursion/TaylorSeriesLoop.java
@@ -3,7 +3,8 @@
  * @email juveriamanzar29@gmail.com
  * @create date 2021-02-17 19:25:12
  * @modify date 2021-02-17 19:25:12
- * @desc e^x=1+x[1+x/2[1+x/3[1+x/4]]]
+ * @desc Taylor Series Horners Rule
+ * e^x=1+x[1+x/2[1+x/3[1+x/4]]]
  * utilizing calling time recursion and hence can be easily written with loops.
  * time complexity: O(n)
  */
--- Recursion/TaylorSeriesRecursion.java
+++ Recursion/TaylorSeriesRecursion.java
@@ -1,27 +1,30 @@
 /**
  * @author Juveria Manzar
  * @email juveriamanzar29@gmail.com
- * @create date 2021-02-17 19:36:44
- * @modify date 2021-02-17 19:36:44
- * @desc e^x=1+x[1+x/2[1+x/3[1+x/4]]]
- * utilizing calling time recursion and hence we would need a static varibale for storing result.
- * time complexity: O(n)
+ * @create date 2021-02-17 20:20:41
+ * @modify date 2021-02-17 20:20:41
+ * @desc Taylor Series recursive call utilizes various recursive functions, sum, factorial, and power 
+ * and as a function can return only single value we ought to utilize static variables.
+ * e^x=1+x+x^2/2!+x^3/3!+....
+ * time complexity: O(n^2)
  */
-
 public class TaylorSeriesRecursion {
-    static double result=1;
-    private static double findTaylorSeries(int n,float x){
+    static double fact=1;
+    static double pow=1;
+    private static double findTaylorSeries(float x,int n){
+        double result;
         if(n==0){
-            return result;
+            return 1;
         }
-        result=1+x/n*result;
-        return findTaylorSeries(n-1, x);
+        result=findTaylorSeries(x,n-1);
+        pow=pow*x;
+        fact=fact*n;
+        return result+pow/fact;
 
     }
     public static void main(String[] args) {
         int n=25;
         float x=5.0f;
-        System.out.println(findTaylorSeries(n,x));
+        System.out.println(findTaylorSeries(x,n));
     }
-
 }
