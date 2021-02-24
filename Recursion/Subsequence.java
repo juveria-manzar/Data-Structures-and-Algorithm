35fcd1bb85d4288e5747eac99d823b41f1dd6bd1
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-02-24 22:41:33 +0530

    Recursion in ArrayList-Subsequence

--- Recursion/Subsequence.java
+++ Recursion/Subsequence.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 
 public class Subsequence {
     private static ArrayList<String> getSubsequence(String str) {
+
         if(str.length()==0){
             ArrayList<String> bres=new ArrayList<>();
             bres.add("");
@@ -17,11 +18,10 @@ public class Subsequence {
 
         //bc->[--,-c,b-,bc]
         //abc->[---,--c,-b-,-bc,a--,a-c,ab-,abc]
-        char ch=str.charAt(0); //a
-        String subS=str.substring(1); //b
-        
-        ArrayList<String> rres=getSubsequence(subS); //bc->[--,-c,b-,bc]
-        
+        char ch=str.charAt(0);
+        String ros=str.substring(1);
+        ArrayList<String> rres=getSubsequence(ros); //bc->[--,-c,b-,bc]
+
         ArrayList<String> mres=new ArrayList<>();
         for(String rstr:rres){
             mres.add(""+rstr);
