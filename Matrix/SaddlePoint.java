4efdaa3febfa41ae75fda38845e905abaa4065fb
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-03-25 23:48:40 +0530

    Matrix Saddle Point

---
+++ Matrix/SaddlePoint.java
@@ -0,0 +1,15 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-03-25 23:48:03
+ * @modify date 2021-03-25 23:48:03
+ * @desc 
+ * 1. You are given a square matrix of size 'n'. You are given n*n elements of the square matrix. 
+ * 2. You are required to find the saddle price of the given matrix and print the saddle price. 
+ * 3. The saddle price is defined as the least price in the row but the maximum price in the column of the matrix.
+ */
+public class SaddlePoint {
+  public static void main(String[] args) {
+      
+  }  
+}
