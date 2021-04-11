fa16ad9774a11bc60bdb5c5fdc2b338b36593836
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-04-11 20:09:28 +0530

    Rotate String

--- Strings/RotateString.java
+++ Strings/RotateString.java
@@ -1,9 +1,13 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-04-11 19:58:10
+ * @modify date 2021-04-11 19:58:10
+ * @desc Given a string A, rotate the string K times in clockwise direction and return the string.
+ */
 public class RotateString {
     public static String solve(String s,int k) {
         k=k%s.length();
-        if(k<0){
-            k=k+s.length();
-        }
         // System.out.println(s.substring(0, s.length()-k));
         // System.out.println(s.substring(s.length()-k));
         String ans=s.substring(s.length()-k)+s.substring(0, s.length()-k);
