9c6243cb4f81c0f0122c9405cd4d02e11a29bd3c
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-04-11 11:46:21 +0530

    Arrays Odd Even Subsequence

---
+++ Arrays/OddEvenSubsequence.java
@@ -0,0 +1,26 @@
+public class OddEvenSubsequence {
+
+    public static int solve(int[] a){
+        int flag=0, count=0;
+
+        if(a[0]%2==0)
+            flag=0;
+        else 
+            flag=1;
+        for(int i=0;i<a.length;i++){
+            if(a[i]%2==0&&flag==0){
+                count++;
+                flag=1;
+            }
+            else if(a[i]%2!=0&&flag==1){
+                count++;
+                flag=0;
+            }
+        }   
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] arr={1, 2, 2, 5, 6};
+        System.out.print(arr);
+    }
+}
