8226a5cf8bdfdb110bbe92056e3b0360a7211c17
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-04-18 23:32:31 +0530

    Sub Array Sum

--- Arrays/SubArraySum.java
+++ Arrays/SubArraySum.java
@@ -10,21 +10,37 @@ import java.util.*;
 
 public class SubArraySum{
 
-    public static int findSubarraySum(int start,int end,int[] arr){
+    public static int findSubarraySum(int start,int end,int[] prefixSum) {
         int sum=0;
-        for(int i=start;i<=end;i++){
-            sum+=arr[i];
-        }
+        if(start==0)
+            sum=prefixSum[end];
+        else 
+            sum=prefixSum[end]-prefixSum[start-1];
         return sum;
+        
     }
+
+    // TC:O(n*q)
+    // public static int findSubarraySum(int start,int end,int[] arr){
+    //     int sum=0;
+    //     for(int i=start;i<=end;i++){
+    //         sum+=arr[i];
+    //     }
+    //     return sum;
+    // }
     public static void main(String[] args) {
         int[] arr={2,3,7,1,9};
         Scanner scn=new Scanner(System.in);
         int q=scn.nextInt();
+        int[] prefixSum=new int[arr.length];
+        prefixSum[0]=arr[0];
+        for(int i=1;i<arr.length;i++){
+            prefixSum[i]=prefixSum[i-1]+arr[i];
+        }
         while(q>0){
             int start=scn.nextInt();
             int end=scn.nextInt();
-            System.out.println(findSubarraySum(start,end,arr));
+            System.out.println(findSubarraySum(start,end,prefixSum));
             q--;
         }
     }
