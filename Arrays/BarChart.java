a5743e6a50b2cc2a0e707c6570a8f277507a5a21
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-04-18 18:45:41 +0530

    Bar Chart Pattern

--- Arrays/BarChart.java
+++ Arrays/BarChart.java
@@ -1,3 +1,12 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-04-17 21:21:08
+ * @modify date 2021-04-17 21:21:08
+ * @desc 1. You are given a number n, representing the size of array a.
+ * 2. You are given n numbers, representing elements of array a.
+ * 3. You are required to print a bar chart representing value of arr a.
+ */
 public class BarChart {
     public static void main(String[] args) {
         int arr[]=new int[]{15,30,40,4,11,9};
