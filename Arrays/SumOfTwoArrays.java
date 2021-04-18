96413053c565ba6aac9f0340ee21b18b3c6337bf
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-04-18 18:36:36 +0530

    Sum of Two Arrays

--- Arrays/SumOfTwoArrays.java
+++ Arrays/SumOfTwoArrays.java
@@ -1,3 +1,15 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-04-18 18:35:14
+ * @modify date 2021-04-18 18:35:14
+ * @desc 1. You are given a number n1, representing the size of array a1.
+ * 2. You are given n1 numbers, representing elements of array a1.
+ * 3. You are given a number n2, representing the size of array a2.
+ * 4. You are given n2 numbers, representing elements of array a2.
+ * 5. The two arrays represent digits of two numbers.
+ * 6. You are required to add the numbers represented by two arrays and print the arrays.
+ */
 public class SumOfTwoArrays {
     public static void main(String[] args) {
         int arr1[]=new int[]{9,9,9};
