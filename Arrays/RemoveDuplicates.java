a51aa0f9904e713aedac5ae1a51d21a0e8d22c80
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-04-19 03:45:05 +0530

    (Two Pointers) Remove Duplicates in Sorted Array

---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-03-13 18:52:40
+ * @modify date 2021-03-13 18:52:40
+ * @desc Remove Duplicates from sorted array.
+ */
+
+public class RemoveDuplicates {
+
+    static int removeDuplicates(int[] arr,int n){
+        int newIndex=0;
+
+        if(n==0||n==1){
+            return n;
+        }
+
+        for(int i=0;i<n-1;i++){
+            if(arr[i]!=arr[i+1]){
+                arr[newIndex]=arr[i];
+                newIndex++;
+            }
+        }
+
+        arr[newIndex++]=arr[n-1];
+        return newIndex;
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1, 2, 2, 3, 4, 4, 4, 5, 5}; 
+        int n=arr.length;
+        n=removeDuplicates(arr,n);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
