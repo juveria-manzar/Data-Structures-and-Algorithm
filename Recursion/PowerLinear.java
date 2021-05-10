c50e063f143fed1befdb6be08ddc86d353a10265
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-05-10 23:30:44 +0530

    Power Linear

--- Recursion/PowerLinear.java
+++ Recursion/PowerLinear.java
@@ -1,15 +1,26 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-05-10 23:28:15
+ * @modify date 2021-05-10 23:28:15
+ * @desc 
+ * 1. You are given a number x.
+ * 2. You are given another number n
+ * 3. You are required to calculate x raised to the power n. Don't change the signature of power function .
+ */
+
 public class PowerLinear {
-    public static int power(int m,int n){
+    public static int power(int x,int n){
         if(n==0){
             return 1;
         }
         else{
-            return power(m, n-1)*m;
+            return power(x,n-1);
         }
     }
     public static void main(String[] args) {
-        int m=0;
+        int x=2;
         int n=5;
-        System.out.println(power(m,n));
+        System.out.println(power(x,n));
     }
 }
