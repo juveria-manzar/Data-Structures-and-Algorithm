8adbcd06217f23d7319e36bdc203bf02abc07362
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-05-11 01:58:37 +0530

    Max of Array

---
+++ Arrays/MaxinArray.java
@@ -0,0 +1,20 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-05-11 01:57:50
+ * @modify date 2021-05-11 01:57:50
+ * @desc Write recursive and not iterative logic
+ */
+public class MaxinArray {
+    public static int max(int[] arr,int index){
+        if(index==arr.length-1){
+            return arr[index];
+        }
+        return Math.max(arr[index],max(arr,index+1));
+    }
+
+    public static void main(String[] args) {
+        int[] arr={65,8,40,2,55};
+        System.out.print(max(arr,0));
+    }
+}
