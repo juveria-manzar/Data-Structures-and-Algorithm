faa29b054e45613f3a965d7bc9cb33144102186f
JUVERIA MANZAR <juveriamanzar29@gmail.com>
2021-05-12 12:12:50 +0530

    All Indices uisng Recursion

---
+++ Arrays/AllIndices.java
@@ -0,0 +1,32 @@
+/**
+ * @author Juveria Manzar
+ * @email juveriamanzar29@gmail.com
+ * @create date 2021-02-19 00:27:32
+ * @modify date 2021-02-19 00:27:32
+ * @desc 
+ */
+
+public class AllIndices {
+
+    public static int[] allIndices(int[] arr,int ele,int index,int fsf) {
+        if(index==arr.length){
+            return new int[fsf];
+        }
+        if(arr[index]==ele){
+            int[] indAr=allIndices(arr, ele, index+1, fsf+1);
+            indAr[fsf]=index;
+            return indAr;
+        }else{
+            int[] indAr=allIndices(arr, ele, index+1, fsf);
+            return indAr;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr={2,3,6,9,8,3,2,3,6,4};
+        int ele=3;
+        int[] iarr=allIndices(arr, ele, 0, 0);
+        for(int i=0;i<iarr.length;i++){
+            System.out.println(iarr[i]);
+        }
+    }
+}
