2d0422100fb2e2ddce827ed9d991257f3b4bcf79
Juveria Manzar <juveriamanzar29@gmail.com>
2021-08-19 07:32:40 +0530

    Recursion-Backtacking

--- Recursion with Backtracking/SubsetSumK.java
+++ Recursion with Backtracking/SubsetSumK.java
@@ -1,21 +1,21 @@
 public class SubsetSumK {
-
-    public static void subsetSumK(int currIndex, int K, int N, int[] arr) {
+    public static boolean subsetSumK(int currIndex, int K, int N, int[] arr) {
         if(currIndex==N){
             if(K==0){
-                System.out.println("true");
+                return true;
             }
-            return;
+            return false;
         }
+        boolean a1=subsetSumK(currIndex+1, K-arr[currIndex], N, arr);
+        boolean a2=subsetSumK(currIndex+1, K, N, arr);
 
-        subsetSumK(currIndex+1, K-arr[currIndex], N, arr);
-        subsetSumK(currIndex+1, K, N, arr);
+        return a1||a2;
     }
     public static void main(String[] args) {
         int N=3;
         int[] arr={5,-2,9};
         int k=7;
 
-        subsetSumK(0,k,N,arr);
+        System.out.println(subsetSumK(0,k,N,arr));
     }
 }
