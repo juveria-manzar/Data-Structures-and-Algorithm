c3995f90ae8b2d2c3571922bad2d03da9a6a46f1
Juveria Manzar <juveriamanzar29@gmail.com>
2021-08-19 06:36:43 +0530

    Recursion-Backtacking

--- Recursion with Backtracking/PrintNumbersOfNDigits2.java
+++ Recursion with Backtracking/PrintNumbersOfNDigits2.java
@@ -1,5 +1,4 @@
 public class PrintNumbersOfNDigits2 {
-
     public static void printAll(int currIndex,int N, int[] digits,int[] arr){
         if(currIndex==N){
             for(int i=0;i<arr.length;i++){
@@ -8,8 +7,10 @@ public class PrintNumbersOfNDigits2 {
             System.out.println();
             return; 
         }
-
         for(int i=0;i<digits.length;i++){
+            if(i==0){
+                continue;
+            }
             arr[currIndex]=digits[i];
             printAll(currIndex+1, N, digits, arr);
         }
