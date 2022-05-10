c7ede9c9360b6b3cddb3a9272fbb755bbcd2c299
Juveria Manzar <juveriamanzar29@gmail.com>
2022-05-11 00:09:50 +0530

    Time: 1 ms (73.61%), Space: 40.9 MB (71.38%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -1,27 +1,26 @@
 class Solution {
-    List<List<Integer>> list;
+    private List<List<Integer>> list;
     public List<List<Integer>> combinationSum3(int k, int n) {
         list = new ArrayList<>();
-        List<Integer> subList = new ArrayList<>();
-        solve(k, n, subList, 1);
+        List<Integer> sublist = new ArrayList<>();
+        solve(k, n, sublist, 1);
         return list;
     }
     
-    void solve(int k, int n, List<Integer> subList, int start){
-        // base case
-        if(n==0 && k==0){
-            list.add(new ArrayList<>(subList));
+    private void solve(int k, int n, List<Integer> sublist, int start){
+        //base case
+        if(k==0&& n==0){
+            list.add(new ArrayList<>(sublist));
             return;
         }
-        if(n<=0 || k<=0)
+        if(n<=0 || k<=0){
             return;
+        }
         
-        // recursive tree
         for(int i=start;i<=9;i++){
-            subList.add(i);
-            solve(k-1, n-i, subList, i+1);
-            // backtracking step
-            subList.remove(subList.size()-1);
+            sublist.add(i);
+            solve(k-1, n-i, sublist, i+1);
+            sublist.remove(sublist.size()-1);
         }
     }
 }
