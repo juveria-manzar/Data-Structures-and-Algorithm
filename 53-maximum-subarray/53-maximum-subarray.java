5eea1652411637334f3aab644f7b90f2d323644b
Juveria Manzar <juveriamanzar29@gmail.com>
2022-06-14 02:22:08 +0530

    Time: 1 ms (100.00%), Space: 51.6 MB (90.54%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Kadanes Algorithm Problem->
+    // We carry the sub array sum as long as it gives us positive sum  
+    // Brute force O(n3)-> O(n2)
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            
+            if(sum>max){
+                max = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
