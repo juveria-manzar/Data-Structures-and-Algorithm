56b10ee1d10b00a557e564a32669be327f1814e3
Juveria Manzar <juveriamanzar29@gmail.com>
2022-08-20 19:33:18 +0530

    Time: 4 ms (97.68%), Space: 60.8 MB (89.52%) - LeetHub

---
+++ 2374-node-with-highest-edge-score/2374-node-with-highest-edge-score.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int edgeScore(int[] edges) {
+        long[] countSum = new long[edges.length];
+        
+        for(int i=0;i<edges.length;i++){
+            countSum[edges[i]]=countSum[edges[i]]+i;
+        }
+        
+        long max=0l;
+        int ans =0 ;
+        
+        for(int i=0;i<edges.length;i++){
+            if(countSum[i]>max){
+                max = countSum[i];
+                ans=i;
+            }
+        }
+        return ans;
+    }
+}
