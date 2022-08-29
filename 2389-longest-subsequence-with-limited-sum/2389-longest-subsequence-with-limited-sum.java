ccdc6b28c0f6d3af82808c9288ba0a21fd30ce39
Juveria Manzar <juveriamanzar29@gmail.com>
2022-08-29 23:47:25 +0530

    Time: 22 ms (40.00%), Space: 51.1 MB (20.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.java
@@ -1,13 +1,26 @@
 class Solution {
-    public int[] answerQueries(int[] A, int[] queries) {
-        Arrays.sort(A);
-        int n = A.length, m = queries.length, res[] = new int[m];
-        for (int i = 1; i < n; ++i)
-            A[i] += A[i - 1];
-        for (int i = 0; i < m; ++i) {
-            int j = Arrays.binarySearch(A, queries[i]);
-            res[i] = Math.abs(j + 1);
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        int[] prefixSum = new int[nums.length];
+        prefixSum[0] = nums[0];
+        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(prefixSum[0], 1);
+        for(int i = 1;i<nums.length;i++){
+            prefixSum[i] = prefixSum[i-1]+nums[i];
+            map.put(prefixSum[i],i+1);
         }
-        return res;
+        
+        int[] ans = new int[queries.length];
+        
+        int i = 0;
+        for(int query: queries){
+            Map.Entry<Integer, Integer> entry = map.floorEntry(query);
+            if(entry!=null){
+                ans[i] = entry.getValue();
+            }
+            i++;
+        }
+        return ans;
     }
 }
