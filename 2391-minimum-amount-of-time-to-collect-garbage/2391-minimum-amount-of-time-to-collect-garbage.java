646c8d97fb23d19b50a4779ab6e433ae159881a7
Juveria Manzar <juveriamanzar29@gmail.com>
2022-08-30 00:53:38 +0530

    Time: 48 ms (33.33%), Space: 88.9 MB (66.67%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int garbageCollection(String[] garbage, int[] travel) {
+        int mLastIndex = 0;
+        int pLastIndex = 0;
+        int gLastIndex = 0;
+        
+        int totalTime = 0;
+        
+        for(int i=0; i<garbage.length;i++){
+            for(char ch: garbage[i].toCharArray()){
+                if(ch=='P') pLastIndex = i;
+                else if(ch=='M') mLastIndex = i;
+                else gLastIndex = i;
+            }
+            totalTime += garbage[i].toCharArray().length;
+        }
+        
+        for(int i=1; i<travel.length;i++){
+            travel[i] = travel[i]+travel[i-1];
+        }        
+        totalTime += pLastIndex == 0 ? 0: travel[pLastIndex-1];
+        totalTime += mLastIndex == 0 ? 0: travel[mLastIndex-1];
+        totalTime += gLastIndex == 0 ? 0: travel[gLastIndex-1];
+        return totalTime;
+    }
+}
