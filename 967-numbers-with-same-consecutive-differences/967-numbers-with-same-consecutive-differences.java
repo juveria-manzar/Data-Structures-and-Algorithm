46545db7b7f8cb28953018afe14e36858c02c762
Juveria Manzar <juveriamanzar29@gmail.com>
2022-09-03 23:32:08 +0530

    Time: 16 ms (7.19%), Space: 44 MB (10.00%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,43 +1,30 @@
 class Solution {
-     // The idea is to use BFS to try appending 0 - 9 to each number 
-    // starting from a single digit 1 - 9 until the number has n digits
     public int[] numsSameConsecDiff(int n, int k) {
-        // push all numbers with single digit to a deque
-        ArrayDeque<Integer> q = new ArrayDeque<>();
         
-        for(int i=1;i<10;i++){
-            q.add(i);
+        HashSet<Integer> output = new HashSet<>(); // Hashset to avoid dupes (e.g k == 0)
+        Queue<Integer> queue = new LinkedList(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        
+        while(!queue.isEmpty()){
+            int currVal = queue.poll();
+            int lastDigit = currVal % 10;
+            int numDigits = (int)Math.log10(currVal) + 1;
+            
+            if(numDigits == n){
+                output.add(currVal);
+                continue;
+            }
+            
+            int nextAddDigit = lastDigit + k;
+            int nextSubDigit = lastDigit - k;
+
+            if(nextAddDigit >= 0 && nextAddDigit <= 9) queue.add((currVal*10) + nextAddDigit);
+            if(nextSubDigit >= 0 && nextSubDigit <= 9) queue.add((currVal*10) + nextSubDigit); 
         }
         
-        while(--n > 0){
-            
-            int sq = q.size();
-            // for each item in the current queue,
-            // do the following logic
-            for (int i = 0; i < sq; i++) {
-                // get the first number from the queue 
-                 // pop it
-                int p = q.remove();
-                // we can potentially add 0 to 9 to the current number p
-                for (int j = 0; j < 10; j++) {
-                    // we use p % 10 to get the last digit of p
-                    // then get the difference with j
-                    // since (p % 10) - j can be negative and positive
-                    // we use abs to cover both case
-                  if (Math.abs((p % 10) - j) == k) {
-                    // if the difference is equal to k
-                    // we can include digit j 
-                    // so multiply the current number by 10 and add j
-                    q.add(p * 10 + j);
-                  }
-                }
-               
-        }
-             // return all numbers in deque, return them in [int]
+        int ind = 0;
+        int[] outputarr = new int[output.size()];
+        for(int i: output){ outputarr[ind++] = i; }
         
-            
-    }
-        int[] integers = q.stream().mapToInt(Integer::intValue).toArray();
-         return integers;
+        return outputarr;
     }
 }
